cfea6fa8685e3e3fcfa20243ac454e66a467aaf6
Shay Ben Moshe <shayfalador@gmail.com>
2022-02-28 19:53:17 +0200

    Add sql to machine

--- deploy-sandbox/internal/create_machine.sh
+++ deploy-sandbox/internal/create_machine.sh
@@ -13,7 +13,9 @@ gcloud compute instances create-with-container $INSTANCE_NAME\
     --tags=http-server\
     --metadata-from-file=startup-script=machine_startup.sh\
     --container-image=$IMAGE_URL\
+    --scopes=default,sql-admin\
     --container-mount-host-path=host-path=/mnt/disks/orig_data,mount-path=/data/orig,mode=ro\
     --container-mount-host-path=host-path=/mnt/disks/ftp_data,mount-path=/data/ftp,mode=ro\
     --container-mount-host-path=host-path=/mnt/disks/ps_cache2,mount-path=/cache/ps_cache2,mode=ro\
+    --container-env=BROWSE_SQLALCHEMY_DATABASE_URI=$(gcloud secrets versions access 1 --secret="sb-mysql-path")\
     --container-env-file=env_values.txt
--- deploy-sandbox/internal/machine_startup.sh
+++ deploy-sandbox/internal/machine_startup.sh
@@ -4,3 +4,8 @@ mkdir -p /mnt/disks/ps_cache2
 mount -o ro 10.36.128.122:/arxiv_orig_data /mnt/disks/orig_data
 mount -o ro 10.131.37.122:/arxiv_ftp_data /mnt/disks/ftp_data
 mount -o ro 10.24.117.242:/arxiv_ps_cache2 /mnt/disks/ps_cache2
+
+docker run -d \
+    -p 127.0.0.1:3306:3306 \
+    gcr.io/cloudsql-docker/gce-proxy:1.19.1 /cloud_sql_proxy \
+    -instances=arxiv-sandbox5:us-central1:arxiv-sandbox5-db1=tcp:0.0.0.0:3306
